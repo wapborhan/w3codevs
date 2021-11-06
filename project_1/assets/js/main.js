66482406530142aa2a931a334cda120687cd9864
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2021-11-07 02:36:08 +0600

    section add

--- project_1/assets/js/main.js
+++ project_1/assets/js/main.js
@@ -3,3 +3,14 @@ $(function () {
   //    mixitup js
   var mixer = mixitup(".course_wrap");
 });
+
+$(document).ready(function () {
+  $(".test-slider").slick({
+    arrows: true,
+    dots: false,
+    dotsClass: "slider-dots",
+    slidesToShow: 1,
+    nextArrow: '<button class="test-arrow testprev"></button>',
+    prevArrow: '<button class="test-arrow testnext"></button>',
+  });
+});
