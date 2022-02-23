42218b6a2597fefc09d80880ee5dfc77b3ac3380
Borhan Uddin <borhaninfos@gmail.com>
2022-02-23 20:09:12 +0600

    delete

--- README.md
+++
@@ -1,3 +0,0 @@
-# w3codevs
-
-All Practice file in <a href="https://wapborhan.github.io/w3codevs/index.html">Here</a>
--- assets/js/script.js
+++
@@ -1,4 +0,0 @@
-function contactAdmin() {
-    alert("Contact Borhan Uddin \n \n \n \n Facebok id: https://www.facebook.com/infoborhan/  \n \n Whatsapp: +8801620557840");
-
-}
